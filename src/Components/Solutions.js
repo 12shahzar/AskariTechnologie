ab950bf88c5237361ffa197133ab1c5af3192ae6
Syed-Abbas-Askari <syed.abbas@askaritechnologiecom>
2023-06-21 07:43:23 -0700

    rfty.

--- src/Components/Solutions.js
+++ src/Components/Solutions.js
@@ -11,6 +11,12 @@ import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
 import BookIcon from '@mui/icons-material/Book';
 import { Link } from "react-router-dom";
+import Asanerp from "../Assets/erplogosvg.svg"
+import CogEdu from "../Assets/Edusvg.svg"
+import Med from "../Assets/medsvg.svg"
+import CC from "../Assets/clientsvg.svg"
+import InstantSales from "../Assets/saleforsvg.svg"
+import Agri from "../Assets/agrisvg.svg"
 
 
 const Solutions = () => {
@@ -30,11 +36,11 @@ const Solutions = () => {
          <Link to="/sol1">
            <div className='row'>
             <div className='col-2'>
-            <ThumbUpAltIcon style={{color:'orange'}}className='biglogo'/> 
+            <img src={Asanerp}/>
             </div>
             <div className='col-10'>
              <h6>Asaan ERP</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <p>Simplify, Streamline, Succeed.</p>
             </div>           
            </div>
            </Link>
@@ -43,11 +49,11 @@ const Solutions = () => {
          <Link to="/sol2">
          <div className='row'>
             <div className='col-2'>
-            <AssignmentIndIcon style={{color:'purple'}}className='biglogo'/> 
+            <img src={CogEdu}/>
             </div>
             <div className='col-10'>
-             <h6>Cognitive Video</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <h6>Cognitive Edu</h6>
+             <p>Empowering Minds, Evolving Education.</p>
             </div>           
            </div>
            </Link>
@@ -57,11 +63,11 @@ const Solutions = () => {
           <Link to="sol3">
          <div className='row'>
             <div className='col-2'>
-            <AssignmentIndIcon style={{color:'purple'}}className='biglogo'/> 
+            <img src={CC}/>
             </div>
             <div className='col-10'>
              <h6>Client Connect</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <p>Seamless Client Engagement.</p>
             </div>           
            </div></Link>
          </div>
@@ -98,11 +104,11 @@ const Solutions = () => {
          <Link to="/sol5">
          <div className='row'>
             <div className='col-2'>
-            <StarHalfIcon style={{color:'seagreen'}}className='biglogo'/> 
+            <img src={InstantSales}/>
             </div>
             <div className='col-10'>
-             <h6>Testimonial</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <h6>Instant Sales</h6>
+             <p>Accelerate Sales Velocity.</p>
             </div>           
            </div>
            </Link>
@@ -111,11 +117,11 @@ const Solutions = () => {
          <Link to="/sol6">
          <div className='row'>
             <div className='col-2'>
-            <BookIcon style={{color:'orange'}}className='biglogo'/> 
+            <img src={Med}/>
             </div>
             <div className='col-10'>
              <h6>Medease</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <p>Streamline Medical Operations.</p>
             </div>           
            </div>
            </Link>
@@ -124,11 +130,11 @@ const Solutions = () => {
           <Link to="sol4">
         <div className='row'>
             <div className='col-2'>
-            <AssignmentIndIcon style={{color:'purple'}}className='biglogo'/> 
+            <img src={Agri}/>
             </div>
             <div className='col-10'>
              <h6>Agrisense</h6>
-             <p>Lorem ipum hdbhfbsd</p>
+             <p>Empowering Agriculture Technology.</p>
             </div>           
            </div></Link>
         </div>
